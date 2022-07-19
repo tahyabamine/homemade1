76409d4ee3fd2449b32c20081abda5624b58813a
tahyabamine <tahya.bamine@gmail.com>
2022-07-19 18:28:17 +0200

    Seo

--- src/Controller/RechercheController.php
+++ src/Controller/RechercheController.php
@@ -8,10 +8,13 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+
 
 class RechercheController extends AbstractController
 {
     #[Route('/recherche', name: 'app_recherche')]
+    #[IsGranted('ROLE_USER')]
     public function index(AnnonceRepository $annoncesRepo, Request $request): Response
     {
         $limite = 4;
